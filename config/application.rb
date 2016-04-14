e220ef091c1bb5655b11ae3225bd4b2b8c76f49b
Mikias Abera <itsmikias@gmail.com>
2016-04-14 11:24:51 +0300

    Create structure.sql, fix the pg_dump -i error by deleting line in postgres_database_tasks.rb file

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Shine
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use SQL for storing the schema
+    config.active_record.schema_format = :sql
   end
 end
