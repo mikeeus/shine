0b7137b4b31aa86877c80c5d9c2364a3060649cc
Mikias Abera <itsmikias@gmail.com>
2016-04-14 12:41:45 +0300

    Add search function

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -11,4 +11,43 @@ class CustomerSearchTerm
       build_for_name_search(search_term)
     end
   end
+
+# Name search ----------------------------------------------
+  def build_for_name_search(search_term)
+    @where_clause << case_insensitive_search(:first_name)
+    @where_args[:first_name] = starts_with(search_term)
+
+    @where_clause << " OR #{case_insensitive_search(:last_name)}"
+    @where_args[:last_name] = starts_with(search_term)
+    
+    @order = "last_name asc"
+  end
+
+  def starts_with(search_term)
+    search_term + "%"
+  end
+
+  def case_insensitive_search(field_name)
+    "lower(#{field_name}) like :#{field_name}"
+  end
+
+
+
+# Email search -------------------------------------------
+  def build_for_email_search(search_term)
+    @where_clause << case_insensitive_search(:first_name)
+    @where_args[:first_name] = starts_with(extract_name(search_term))
+
+    @where_clause << " OR #{case_insensitive_search(:last_name)}"
+    @where_args[:last_name] = starts_with(extract_name(search_term))
+
+    @where_clause << " OR #{case_insensitive_search(:email)}"
+    @where_args[:email] = search_term
+
+    @order = "lower(email) = " + ActiveRecord::Base.connection.quote(search_term) + "desc, last_name asc"
+  end
+
+  def extract_name(email)
+    email.gsub(/@.*$/,'').gsub(/[0-9]+/,'')   
+  end
 end
